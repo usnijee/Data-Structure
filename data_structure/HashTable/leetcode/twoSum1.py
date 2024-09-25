1718359a1d0c9d022f357fb2a5cb73cf1d4653d8
LEE JINSU <bluesea9519@gmail.com>
2024-09-25 10:43:28 +0900

    UPDATE: start hashtable

--- data_structure/HashTable/leetcode/twoSum1.py
+++ data_structure/HashTable/leetcode/twoSum1.py
@@ -8,8 +8,26 @@ Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 '''
 
-# class Solution(object):
-#     def twoSum(self, nums, target):
+class Solution(object):
+    def twoSum(self, nums, target):
+        table = {value:idx for idx, value in enumerate(nums)} 
+
+        for i, num in enumerate(nums):
+            if ((target-num) in table) and (i != table[(target-num)]):
+                return [i, table[(target-num)]]
+
+
+'''
+접근 방법에 대해 생각해볼것
+
+1. 해쉬테이블을 떠올릴수 있는 조건 
+
+    1-1. 아무 순서로 반환 가능 (순서를 고려하지 않아도 됨)
+    1-2. 해시 테이블은 접근/삽입/삭제에 대해 O(1)임을 항상 인지할 것  --> 즉, 순서만 자유롭다면 대상에 접근해서 삽입하거나 삭제하는 기능이 매우 빠름
+    1-3. in연산자를 통해 1-2를 빠르게 구현 가능 
+
+
+    즉, 순서를 고려하지 않아도 된다면 해시테이블을 떠올릴것
+    또한 리스트가 필요하다면 리스트 컴프리헨션은 반드시 떠올릴것
+'''
         
-k = [1,3,4]
-print(dict(k))
