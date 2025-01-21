388eff5c76b49eab5e84915e9f79a8094cc03abd
LEE JINSU <bluesea9519@gmail.com>
2025-01-21 22:39:13 +0900

    feat: 백준 1021(회전하는 큐)

---
+++ java/data_structure/Queue/baekjoon/src/baekjoon1021.java
@@ -0,0 +1,82 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class baekjoon1021 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        int n = Integer.parseInt(scanner.next());
+        int m = Integer.parseInt(scanner.next());
+        scanner.nextLine();
+        String[] nums = scanner.nextLine().split(" ");
+
+        for (int i = 1; i <= n; i++) {
+            deque.offerLast(i);
+        }
+
+        int idx = 0;
+        int minSum = 0;
+        boolean isCloseFirst;
+
+        while (idx < m) {
+            int selectNum = Integer.parseInt(nums[idx]);
+            isCloseFirst = compareMidOf(selectNum, deque);
+            int cnt1 = 0;
+            int cnt2 = 0;
+
+            if (isCloseFirst) {
+                while (true) {
+                    if (deque.getFirst() == selectNum) {
+                        deque.pollFirst();
+                        break;
+                    }
+                    makeCW(deque);
+                    cnt1++;
+                }
+            } else if (!isCloseFirst) {
+
+                while (true) {
+                    if (deque.getFirst() == selectNum) {
+                        deque.pollFirst();
+                        break;
+                    }
+                    makeCounterCW(deque);
+                    cnt2++;
+                }
+            }
+
+            minSum += cnt1 > cnt2 ? cnt1 : cnt2;
+            idx++;
+        }
+        System.out.println(minSum);
+    }
+
+    private static boolean compareMidOf(int selectNum, Deque<Integer> deque) {
+        ArrayList<Integer> dequeAsList = new ArrayList<>(deque);
+        int middle = dequeAsList.get(deque.size() / 2);
+
+        if (selectNum < middle) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static void makeCounterCW(Deque<Integer> deque) {
+        int num = deque.pollLast();
+        deque.offerFirst(num);
+    }
+
+    private static void makeCW(Deque<Integer> deque) {
+        int num = deque.pollFirst();
+        deque.offerLast(num);
+    }
+
+
+
+}
