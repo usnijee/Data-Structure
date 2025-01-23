80b886a721f97b91e94effba86fea7c1df15b9d5
JINSU LEE <bluesea9519@gmail.com>
2025-01-23 20:55:18 +0900

    style: temporary upload

---
+++ java/data_structure/HashMap/baekjoon/src/bj26008.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class bj26008 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.next());
+        int m = Integer.parseInt(scanner.next());
+        int a = Integer.parseInt(scanner.next());
+        int h = scanner.nextInt();
+
+        int r = 0;
+        for
+    }
+}
