58d5695d38b1656a31393fa5c68adac37f46ed9c
JINSU LEE <bluesea9519@gmail.com>
2025-02-09 15:33:21 +0900

    feat: temporary upload

---
+++ java/algorithm/src/sort/beakjoon/bj2751.java
@@ -0,0 +1,15 @@
+package sort.beakjoon;
+
+import java.util.Scanner;
+
+public class bj2751 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String n = scanner.nextLine();
+
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+}
